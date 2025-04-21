a6cf50bc54b96e08df531c8832c926ac880992b9
İsmail Perim <ismail@perim.net>
2025-04-21 16:58:03 +0300

    Add ops-p01 added

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,15 @@ module "server-vpn-p01" {
   clone_template      = var.clone_template
   ssh_keys            = var.ssh_keys
 }
+
+module "server-ops-p01" {
+  source              = "./servers/ops-p01"
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  target_node         = var.target_node
+  gateway             = var.gateway
+  storage             = var.storage
+  clone_template      = var.clone_template
+  ssh_keys            = var.ssh_keys
+}
